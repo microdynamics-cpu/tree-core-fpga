82bb3fe3a2f51b552157a5e5b77fa0bea0bfe97c
maksyuki <maksyuki@126.com>
2023-02-24 20:55:58 +0800

    feat: add open source 32bits axi4 bridge impl

---
+++ gowin/spinal/hw/spinal/axi4ddr3/Config.scala
@@ -0,0 +1,14 @@
+package axi4ddr3
+
+import spinal.core._
+import spinal.core.sim._
+
+object Config {
+  def spinal = SpinalConfig(
+    targetDirectory = "hw/gen"
+  )
+}
+
+object GOWIN_AXI4_DDR3 extends App {
+    Config.spinal.generateVerilog(Paski_GowinDDR_AXI4(ClockDomain.external("sys_clk"),ClockDomain.external("mem_clk")))
+}
