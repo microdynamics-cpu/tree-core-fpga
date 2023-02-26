aecac8f69ec87448b74464c668fa9cf410ae57bd
maksyuki <maksyuki@126.com>
2023-02-26 21:16:16 +0800

    style: format the codes

--- gowin/spinal/hw/spinal/axi4ddr3/Gowin_DDR3.scala
+++ gowin/spinal/hw/spinal/axi4ddr3/Gowin_DDR3.scala
@@ -3,7 +3,7 @@ package axi4ddr3
 import spinal.core._
 import spinal.lib.master
 
-case class Gowin_DDR3(sysclk : ClockDomain, memclk : ClockDomain) extends BlackBox {
+case class Gowin_DDR3(sysclk: ClockDomain, memclk: ClockDomain) extends BlackBox {
 
   val io = new Bundle {
     /*
@@ -15,7 +15,7 @@ case class Gowin_DDR3(sysclk : ClockDomain, memclk : ClockDomain) extends BlackB
 		.cmd_ready(cmd_ready_o), //output cmd_ready
 		.cmd(cmd_i), //input [2:0] cmd
 		.cmd_en(cmd_en_i), //input cmd_en
-		.addr(addr_i), //input [26:0] addr
+		.addr(addr_i), //input [26:0] addr // ip: [27:0]
 		.wr_data_rdy(wr_data_rdy_o), //output wr_data_rdy
 		.wr_data(wr_data_i), //input [127:0] wr_data
 		.wr_data_en(wr_data_en_i), //input wr_data_en
@@ -32,7 +32,7 @@ case class Gowin_DDR3(sysclk : ClockDomain, memclk : ClockDomain) extends BlackB
 		.clk_out(clk_out_o), //output clk_out
 		.ddr_rst(ddr_rst_o), //output ddr_rst
 		.burst(burst_i), //input burst
-		.O_ddr_addr(O_ddr_addr_o), //output [12:0] O_ddr_addr
+		.O_ddr_addr(O_ddr_addr_o), //output [12:0] O_ddr_addr // ip: [13:0]
 		.O_ddr_ba(O_ddr_ba_o), //output [2:0] O_ddr_ba
 		.O_ddr_cs_n(O_ddr_cs_n_o), //output O_ddr_cs_n
 		.O_ddr_ras_n(O_ddr_ras_n_o), //output O_ddr_ras_n
@@ -47,68 +47,70 @@ case class Gowin_DDR3(sysclk : ClockDomain, memclk : ClockDomain) extends BlackB
 		.IO_ddr_dq(IO_ddr_dq_io), //inout [15:0] IO_ddr_dq
 		.IO_ddr_dqs(IO_ddr_dqs_io), //inout [1:0] IO_ddr_dqs
 		.IO_ddr_dqs_n(IO_ddr_dqs_n_io) //inout [1:0] IO_ddr_dqs_n
-    */
-    val memory_clk = in Bool()
-    val clk = in Bool()
-    val pll_lock = in Bool()
-    val rst_n = in Bool()
-    val app_burst_number = in UInt(6 bits)
-    val cmd_ready = out Bool()
-    val cmd = in Bits(3 bits)
-    val cmd_en = in Bool()
-    val addr = in UInt (27 bits)
-    val wr_data_rdy = out Bool()
-    val wr_data = in Bits(128 bits)
-    val wr_data_en = in Bool()
-    val wr_data_end = in Bool()
-    val wr_data_mask = in Bits(16 bits)
-    val rd_data = out Bits(128 bits)
-    val rd_data_valid = out Bool()
-    val rd_data_end = out Bool()
-    val sr_req = in Bool()
-    val ref_req = in Bool()
-    val sr_ack = out Bool()
-    val ref_ack = out Bool()
-    val init_calib_complete = out Bool()
-    val clk_out = out Bool()
-    val ddr_rst = out Bool()
-    val burst = in Bool()
+     */
+    val memory_clk       = in.Bool()
+    val clk              = in.Bool()
+    val pll_lock         = in.Bool()
+    val rst_n            = in.Bool()
+    val app_burst_number = in.UInt(6 bits)
+    val cmd_ready        = out.Bool()
+    val cmd              = in.Bits(3 bits)
+    val cmd_en           = in.Bool()
+    val addr             = in.UInt(27 bits)
+    // val addr = in UInt (28 bits)
+    val wr_data_rdy         = out.Bool()
+    val wr_data             = in.Bits(128 bits)
+    val wr_data_en          = in.Bool()
+    val wr_data_end         = in.Bool()
+    val wr_data_mask        = in.Bits(16 bits)
+    val rd_data             = out.Bits(128 bits)
+    val rd_data_valid       = out.Bool()
+    val rd_data_end         = out.Bool()
+    val sr_req              = in.Bool()
+    val ref_req             = in.Bool()
+    val sr_ack              = out.Bool()
+    val ref_ack             = out.Bool()
+    val init_calib_complete = out.Bool()
+    val clk_out             = out.Bool()
+    val ddr_rst             = out.Bool()
+    val burst               = in.Bool()
 
-    val O_ddr_addr = out Bits(13 bits)
-    val O_ddr_ba = out Bits(3 bits)
-    val O_ddr_cs_n = out Bool()
-    val O_ddr_ras_n = out Bool()
-    val O_ddr_cas_n = out Bool()
-    val O_ddr_we_n = out Bool()
-    val O_ddr_clk = out Bool()
-    val O_ddr_clk_n = out Bool()
-    val O_ddr_cke = out Bool()
-    val O_ddr_odt = out Bool()
-    val O_ddr_reset_n = out Bool()
-    val O_ddr_dqm = out Bits(2 bits)
-    val IO_ddr_dq = inout Bits(16 bits)
-    val IO_ddr_dqs = inout Bits(2 bits)
-    val IO_ddr_dqs_n = inout Bits(2 bits)
+    val O_ddr_addr = out.Bits(13 bits)
+    // val O_ddr_addr = out Bits(14 bits)
+    val O_ddr_ba      = out.Bits(3 bits)
+    val O_ddr_cs_n    = out.Bool()
+    val O_ddr_ras_n   = out.Bool()
+    val O_ddr_cas_n   = out.Bool()
+    val O_ddr_we_n    = out.Bool()
+    val O_ddr_clk     = out.Bool()
+    val O_ddr_clk_n   = out.Bool()
+    val O_ddr_cke     = out.Bool()
+    val O_ddr_odt     = out.Bool()
+    val O_ddr_reset_n = out.Bool()
+    val O_ddr_dqm     = out.Bits(2 bits)
+    val IO_ddr_dq     = inout.Bits(16 bits)
+    val IO_ddr_dqs    = inout.Bits(2 bits)
+    val IO_ddr_dqs_n  = inout.Bits(2 bits)
 
     //val ddr_iface = master(DDR3_Interface())
   }
 
   def connectDDR3Interface(iface: DDR3_Interface): Unit = {
-    iface.O_ddr_addr := io.O_ddr_addr
-    iface.O_ddr_ba := io.O_ddr_ba
-    iface.O_ddr_cs_n := io.O_ddr_cs_n
-    iface.O_ddr_ras_n := io.O_ddr_ras_n
-    iface.O_ddr_cas_n := io.O_ddr_cas_n
-    iface.O_ddr_we_n := io.O_ddr_we_n
-    iface.O_ddr_clk := io.O_ddr_clk
-    iface.O_ddr_clk_n := io.O_ddr_clk_n
-    iface.O_ddr_cke := io.O_ddr_cke
-    iface.O_ddr_odt := io.O_ddr_odt
+    iface.O_ddr_addr    := io.O_ddr_addr
+    iface.O_ddr_ba      := io.O_ddr_ba
+    iface.O_ddr_cs_n    := io.O_ddr_cs_n
+    iface.O_ddr_ras_n   := io.O_ddr_ras_n
+    iface.O_ddr_cas_n   := io.O_ddr_cas_n
+    iface.O_ddr_we_n    := io.O_ddr_we_n
+    iface.O_ddr_clk     := io.O_ddr_clk
+    iface.O_ddr_clk_n   := io.O_ddr_clk_n
+    iface.O_ddr_cke     := io.O_ddr_cke
+    iface.O_ddr_odt     := io.O_ddr_odt
     iface.O_ddr_reset_n := io.O_ddr_reset_n
-    iface.O_ddr_dqm := io.O_ddr_dqm
-    iface.IO_ddr_dq := io.IO_ddr_dq
-    iface.IO_ddr_dqs := io.IO_ddr_dqs
-    iface.IO_ddr_dqs_n := io.IO_ddr_dqs_n
+    iface.O_ddr_dqm     := io.O_ddr_dqm
+    iface.IO_ddr_dq     := io.IO_ddr_dq
+    iface.IO_ddr_dqs    := io.IO_ddr_dqs
+    iface.IO_ddr_dqs_n  := io.IO_ddr_dqs_n
   }
 
   noIoPrefix()
@@ -116,6 +118,6 @@ case class Gowin_DDR3(sysclk : ClockDomain, memclk : ClockDomain) extends BlackB
   mapClockDomain(memclk, clock = io.memory_clk)
   setDefinitionName("DDR3_Memory_Interface_Top")
 
-  val clk_out = ClockDomain(clock = io.clk_out, reset = io.ddr_rst, config = ClockDomainConfig(resetKind = ASYNC, resetActiveLevel = HIGH), frequency = FixedFrequency(150 MHz))
+  val clk_out = ClockDomain(clock = io.clk_out, reset = io.ddr_rst, config = ClockDomainConfig(resetKind = ASYNC, resetActiveLevel = HIGH), frequency = FixedFrequency(100 MHz))
 
 }
--- gowin/spinal/hw/spinal/axi4ddr3/Paski_GowinDDR14_Controller.scala
+++ gowin/spinal/hw/spinal/axi4ddr3/Paski_GowinDDR14_Controller.scala
@@ -1,63 +1,63 @@
 package axi4ddr3
 
 import spinal.core._
-import spinal.lib.{Stream, StreamFifoCC, master, slave}
+import spinal.lib.{master, slave, Stream, StreamFifoCC}
 
 object Paski_GowinDDR_CMDTYPE extends SpinalEnum {
   val read, write = newElement()
 }
 
-case class Paski_GowinDDR_PayloadCMD[T <: Data](addrlen: Int, burstlen: Int, contextType : T) extends Bundle {
-  val cmdtype = Paski_GowinDDR_CMDTYPE()
-  val addr = UInt(addrlen bits)
+case class Paski_GowinDDR_PayloadCMD[T <: Data](addrlen: Int, burstlen: Int, contextType: T) extends Bundle {
+  val cmdtype   = Paski_GowinDDR_CMDTYPE()
+  val addr      = UInt(addrlen bits)
   val burst_cnt = UInt(burstlen bits)
-  val wr_data = Bits(128 bits)
-  val wr_mask = Bits(16 bits)
-  val context = cloneOf(contextType)
+  val wr_data   = Bits(128 bits)
+  val wr_mask   = Bits(16 bits)
+  val context   = cloneOf(contextType)
 }
 
-case class Paski_GowinDDR_PayloadRSP[T <: Data](contextType : T) extends Bundle {
+case class Paski_GowinDDR_PayloadRSP[T <: Data](contextType: T) extends Bundle {
   val rsp_data = Bits(128 bits)
-  val context = cloneOf(contextType)
+  val context  = cloneOf(contextType)
 }
 
-case class Paski_GowinDDR14_Controller[T <: Data](sys_clk: ClockDomain, ddr_ref_clk: ClockDomain, addrlen: Int = 27, burstlen: Int = 6, fifo_length: Int = 32, contextType : T) extends Component{
+case class Paski_GowinDDR14_Controller[T <: Data](sys_clk: ClockDomain, ddr_ref_clk: ClockDomain, addrlen: Int = 27, burstlen: Int = 6, fifo_length: Int = 32, contextType: T) extends Component {
 
-  val sys_clk_inst = sys_clk
+  val sys_clk_inst     = sys_clk
   val ddr_ref_clk_inst = ddr_ref_clk
 
   val io = new Bundle() {
-    val app_burst_number = out UInt(burstlen bits)
-    val cmd_ready = in Bool()
-    val cmd = out Bits(3 bits)
-    val cmd_en = out Bool()
-    val addr = out UInt (addrlen bits)
-    val wr_data_rdy = in Bool()
-    val wr_data = out Bits(128 bits)
-    val wr_data_en = out Bool()
+    val app_burst_number = out.UInt(burstlen bits)
+    val cmd_ready        = in.Bool()
+    val cmd              = out.Bits(3 bits)
+    val cmd_en           = out.Bool()
+    val addr             = out.UInt(addrlen bits)
+    val wr_data_rdy      = in.Bool()
+    val wr_data          = out.Bits(128 bits)
+    val wr_data_en       = out.Bool()
     // val wr_data_end = out Bool() // not use due to 1:4 mode
-    val wr_data_mask = out Bits(16 bits)
-    val rd_data = in Bits(128 bits)
-    val rd_data_valid = in Bool()
+    val wr_data_mask  = out.Bits(16 bits)
+    val rd_data       = in.Bits(128 bits)
+    val rd_data_valid = in.Bool()
     // val rd_data_end = in Bool() // not use due to 1:4 mode
-    val init_calib_complete = in Bool()
+    val init_calib_complete = in.Bool()
 
     val ddr_cmd = slave(Stream(Paski_GowinDDR_PayloadCMD(addrlen, burstlen, contextType)))
     val ddr_rsp = master(Stream(Paski_GowinDDR_PayloadRSP(contextType)))
   }
 
   val cmd_fifo = new StreamFifoCC(
-    dataType = Paski_GowinDDR_PayloadCMD(addrlen, burstlen, contextType),
-    depth = fifo_length,
+    dataType  = Paski_GowinDDR_PayloadCMD(addrlen, burstlen, contextType),
+    depth     = fifo_length,
     pushClock = sys_clk,
-    popClock = ddr_ref_clk
+    popClock  = ddr_ref_clk
   )
 
   val rsp_fifo = new StreamFifoCC(
-    dataType = Paski_GowinDDR_PayloadRSP(contextType),
-    depth = fifo_length,
+    dataType  = Paski_GowinDDR_PayloadRSP(contextType),
+    depth     = fifo_length,
     pushClock = ddr_ref_clk,
-    popClock = sys_clk
+    popClock  = sys_clk
   )
 
   val sys_area = new ClockingArea(sys_clk) {
@@ -66,14 +66,14 @@ case class Paski_GowinDDR14_Controller[T <: Data](sys_clk: ClockDomain, ddr_ref_
   }
 
   val ddr_control_area = new ClockingArea(ddr_ref_clk) {
-    val cmd_free = Reg(Bool()) init True
-    val cmd_can_send = RegNext(io.cmd_ready & io.wr_data_rdy & io.init_calib_complete) init False
-    val cmd_trigger = Reg(Bool()) init False
-    val cmd_en = cmd_trigger
-    val burst_cnt = Reg(UInt((burstlen + 1) bits)) init 0
+    val cmd_free     = Reg(Bool()).init(True)
+    val cmd_can_send = RegNext(io.cmd_ready & io.wr_data_rdy & io.init_calib_complete).init(False)
+    val cmd_trigger  = Reg(Bool()).init(False)
+    val cmd_en       = cmd_trigger
+    val burst_cnt    = Reg(UInt((burstlen + 1) bits)).init(0)
 
-    val rd_fire = io.rd_data_valid
-    val wr_fire = io.wr_data_en & io.wr_data_rdy
+    val rd_fire   = io.rd_data_valid
+    val wr_fire   = io.wr_data_en & io.wr_data_rdy
     val data_fire = rd_fire | wr_fire
 
     cmd_fifo.io.pop.ready := cmd_free & cmd_can_send
@@ -89,28 +89,27 @@ case class Paski_GowinDDR14_Controller[T <: Data](sys_clk: ClockDomain, ddr_ref_
       cmd_trigger.clear()
     }
     val cmd = RegNextWhen(cmd_fifo.io.pop.payload, cmd_fifo.io.pop.fire)
-    cmd.burst_cnt init 1
+    cmd.burst_cnt.init(1)
     when(burst_cnt === cmd.burst_cnt +^ 1) {
       cmd_free.setWhen(cmd_free === False)
     }
 
-    io.cmd_en := cmd_en
+    io.cmd_en           := cmd_en
     io.app_burst_number := cmd.burst_cnt
-    io.addr := cmd.addr
-    io.wr_data := cmd.wr_data
-    io.wr_data_mask := cmd.wr_mask
+    io.addr             := cmd.addr
+    io.wr_data          := cmd.wr_data
+    io.wr_data_mask     := cmd.wr_mask
     when(cmd.cmdtype === Paski_GowinDDR_CMDTYPE.read) {
-      io.cmd := B"001"
+      io.cmd        := B"001"
       io.wr_data_en := False
     }.otherwise { // write
-      io.cmd := B"000"
+      io.cmd        := B"000"
       io.wr_data_en := (burst_cnt =/= cmd.burst_cnt +^ 1) & io.wr_data_rdy
     }
 
-    rsp_fifo.io.push.valid := io.rd_data_valid
+    rsp_fifo.io.push.valid            := io.rd_data_valid
     rsp_fifo.io.push.payload.rsp_data := io.rd_data
-    rsp_fifo.io.push.payload.context := cmd.context
+    rsp_fifo.io.push.payload.context  := cmd.context
   }
-
 
 }
